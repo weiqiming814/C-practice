093916ae0835663a69ac8a7e8884fd0eb9704ecd
‘wqm’ <weiqiming814@gmail.com>
2023-10-12 10:12:42 +0000

    feat: 浏览器连接

--- web/web_v2/server.c
+++ web/web_v2/server.c
@@ -35,7 +35,6 @@ static MY_HTTPD_CONF conf;
 int read_config(const char *filename)
 {
 	char line[256];
-	char *p;
 
 	if (filename == NULL)
 	{
@@ -59,7 +58,7 @@ int read_config(const char *filename)
 		{
 			conf.port = atoi(value);
 		}
-		else if(strcmp(key, "Directory") == 0)
+		else if (strcmp(key, "Directory") == 0)
 		{
 			strncpy(conf.root_dir, value, strlen(value) - 1);
 		}
@@ -70,33 +69,132 @@ int read_config(const char *filename)
 	return 0;
 }
 
+static char *get_url(const char* request)
+{
+	char buf[MAX_BUFFER] = {0};
+	char *url;
+
+	strncpy(buf, request, strlen(request));
+	strtok(buf, " ");
+	url = strtok(NULL, " ");
+	char *ret = malloc(strlen(url) + 1);
+	if (url[0] == '/' && url[1] == '\0')
+	{
+		strcpy(ret, "/index.html");
+	}
+	else
+	{
+		strcpy(ret, url);
+	}
+	return ret;
+}
+
+int file_exist(const char* filename)
+{
+	FILE *file;
+	if ((file = fopen(filename, "r")) != NULL)
+	{
+		return 1;
+	}
+	fclose(file);
+	return 0;
+}
+
+static char *get_content_type(char *url)
+{
+	char *ext = strstr(url, ".");
+	char *ret = malloc(strlen(url) + 1);
+
+	if (strcmp(ext, ".html") == 0)
+	{
+		strcpy(ret, "text/html");
+	}
+	else if (strcmp(ext, ".jpg") == 0)
+	{
+		strcpy(ret, "image/jpeg");
+	}
+	else if (strcmp(ext, "ico") == 0)
+	{
+		strcpy(ret, "image/x-icon");
+	}
+	else
+	{
+		strcpy(ret, "text/html");
+	}
+	return ret;
+}
+
+static long get_content_length(char *filename)
+{
+	FILE *fp = NULL;
+
+	if ((fp = fopen(filename, "r")) == NULL)
+	{
+		printf("获取失败");
+		return -1;
+	}
+
+	fseek(fp, 0, SEEK_END);
+	long file_size = ftell(fp);
+	fclose(fp);
+	return file_size;
+}
+
+static char *get_head(char *request)
+{
+	char index_file[256];
+	char *url = get_url(request);
+	snprintf(index_file, sizeof(index_file), "%s%s", conf.root_dir, url);
+	char *content_type = get_content_type(url);
+	long content_length = get_content_length(index_file);
+	FILE *file = fopen(index_file, "rb");
+	char *head = (char *)malloc(MAX_BUFFER);
+	if (file_exist(index_file))
+	{
+		snprintf(head, MAX_BUFFER, "HTTP/1.1 200 OK\r\nContent-Type: %s\r\n"
+						"Content-Length: %ld\r\n"
+						"\r\n",
+						content_type, content_length);
+	}
+	else
+	{
+		snprintf(head, MAX_BUFFER, "HTTP/1.1 404 Not Found\r\nContent-Type: %s\r\n"
+						"\r\nrequest file not found\r\n",
+						content_type);
+	}
+	return head;
+}
+
+void do_cat(int fd, FILE *file)
+{
+	int c;
+
+	while ((c = getc(file)) != EOF)
+	{
+		write(fd, &c, 1);
+	}
+}
+
 static void start_server(MY_HTTPD_CONF conf)
 {
 	int server_fd, client_fd;
 	struct sockaddr_in address;
 	int addrlen = sizeof(address);
 	char buffer[MAX_BUFFER] = {0};
-	char response[MAX_BUFFER] = "HTTP/1.1 200 OK\nContent-Type: text/html\n\n";
-	char index_file[256];
-
-	strcpy(index_file, conf.root_dir);
-	strcat(index_file, "/index.html");
-
-	FILE *file = fopen(index_file, "r");
-	if (file != NULL)
-	{
-		fread(response, 1, MAX_BUFFER - strlen(response), file);
-		fclose(file);
-	}
-	else
-	{
-		strcat(response, "<html><body><h1>404 Not Found</h1></body></html>\n");
-	}
 
 	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
 	{
 		perror("socket failed");
 		exit(EXIT_FAILURE);
+	}
+
+	// 解决close后的wait_time
+	int val = 1;
+	int ret = setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, (void *)&val, sizeof(int));
+	if (ret == -1)
+	{
+		printf("setsockopt");
+		exit(1);
 	}
 
 	address.sin_family      = AF_INET;
@@ -128,21 +226,31 @@ static void start_server(MY_HTTPD_CONF conf)
 		}
 
 		read(client_fd, buffer, MAX_BUFFER);
+		char *head = get_head(buffer);
+
 		printf("%s\n", buffer);
-
-		write(client_fd, response, strlen(response));
-		close(client_fd);
-
-		// 解决close后的wait_time
-		int val = 1;
-		int ret = setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, (void *)&val, sizeof(int));
-		if (ret == -1)
+		
+		char *url = get_url(buffer);
+		char file_name[256];
+		snprintf(file_name, sizeof(file_name), "%s%s", conf.root_dir, url);
+		
+		FILE *file = fopen(file_name, "rb");
+		if (file != NULL)
 		{
-			printf("setsockopt");
-			exit(1);
+			write(client_fd, head, strlen(head));
+			do_cat(client_fd, file);
+			fclose(file);
 		}
+		else
+		{
+			write(client_fd, head, strlen(head));
+		}
+
+		close(client_fd);
+		free(head);
 	}
 }
+
 
 int main(int argc, char *argv[])
 {
@@ -155,4 +263,3 @@ int main(int argc, char *argv[])
 
 	return 0;
 }
-
